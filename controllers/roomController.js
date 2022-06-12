86e0cdee0497582850e09667a65e4b64b32c5308
Nugie Jaya Nugraha <90244101+jayanugie@users.noreply.github.com>
2022-06-12 19:19:04 +0700

    Delete controllers directory

--- controllers/roomController.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-const { room } = require('../models');
-
-// membuat room, input nama ruangan, memakain token yg sudah register
-router.post('/room', (req, res) => {
-    
-})
